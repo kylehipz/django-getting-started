112341000ec39e8ddace7ce0412819a9e70d0030
Kyle Hipolito <kylehipolito2109@gmail.com>
2023-12-14 13:36:02 +0800

    feat: Create question and choice models

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponse, render
 
 # Create your views here.
+
+
+def index(request):
+    text = bytes("Hello, world!!!! This is from kyle", "utf-8")
+    return HttpResponse(text)
